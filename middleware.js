910af3706a8079d9423e5c6aa0b6230316000dee
takayasu.sagawa <takayasu.sagawa@i-studio.co.jp>
2026-04-09 16:45:19 +0900

    Add Basic auth middleware

---
+++ middleware.js
@@ -0,0 +1,25 @@
+export const config = {
+  matcher: ["/:path*"],
+};
+
+export default function middleware(req) {
+  const basicAuth = req.headers.get("authorization");
+
+  if (basicAuth) {
+    const [scheme, encoded] = basicAuth.split(" ");
+    if (scheme === "Basic") {
+      const decoded = atob(encoded);
+      const [user, password] = decoded.split(":");
+      if (user === "portfolio" && password === "tyuhbnm") {
+        return new Response(null, { status: 200 });
+      }
+    }
+  }
+
+  return new Response("Unauthorized", {
+    status: 401,
+    headers: {
+      "WWW-Authenticate": 'Basic realm="Secure Area"',
+    },
+  });
+}
